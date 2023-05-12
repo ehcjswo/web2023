235d150ac41d5f6b88bb66b2ccccbda928767c19
KimDoHyeon <ehcjswo1@gmail.com>
2023-05-12 16:18:50 +0900

    05-12

---
+++ webd/webdTest/script/script.js
@@ -0,0 +1,51 @@
+$(function(){
+    // 메뉴 : 단일
+    // $(".header__right > ul > li").mouseover(function(){
+    //     $(this).find(".submenu").stop().slideDown(200);
+    // });
+    // $(".header__right > ul > li").mouseout(function(){
+    //     $(this).find(".submenu").stop().slideUp(200);
+    // });
+
+    $(".header__right > ul > li").on({
+        mouseover: function() {
+            $(".header__right > ul > li").find(".submenu").stop().slideDown(200);
+        },
+        focusin: function() {
+            $(this).find(".submenu").stop().slideDown(200);
+        },
+        mouseout: function() {
+            $(".header__right > ul > li").find(".submenu").stop().slideUp(200);
+        },
+        focusout: function() {
+            $(this).find(".submenu").stop().slideUp(200);
+        }
+    });
+
+    // 슬라이드 : 페이드 효과
+    let currentIndex = 0;
+
+    const $slider = $(".slider");
+    $slider.hide().first().show();  //모든 이미지 숨기고 첫번째 이미지 등장
+
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % $slider.length;
+
+        $slider.eq(currentIndex).fadeOut(300);
+        $slider.eq(nextIndex).fadeIn(300);
+
+        currentIndex = nextIndex;
+    }, 3000)
+
+    // 탭 메뉴
+    const tabBtn = $(".info-menu > a");
+    const tabCont = $(".info-cont > ul");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.on("click", function(){
+        const index = $(this).index();
+        
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+});
